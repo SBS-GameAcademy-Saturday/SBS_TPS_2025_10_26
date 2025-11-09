10a5ff5c64bb2b06a88045f1d3e7a535b0219078
SeoBYP <jajazxcv@naver.com>
2025-11-09 18:50:57 +0900

    2025-11-09 수업자료

--- Assets/Scripts/EnemyHeadlthBar.cs
+++ Assets/Scripts/EnemyHeadlthBar.cs
@@ -6,13 +6,23 @@ public class EnemyHeadlthBar : MonoBehaviour
     [SerializeField] private Slider healthBar;
     [SerializeField] private Damagable damagable;
 
+    private Transform player;
+
     private void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         damagable.OnHealthChangedEvent.AddListener(OnHealthChanaged);
+    }
+
+    private void Update()
+    {
+        transform.LookAt(player);
     }
 
     private void OnHealthChanaged(float currentHealth, float maxHealth)
     {
         healthBar.value = currentHealth / maxHealth;
     }
+
+
 }
